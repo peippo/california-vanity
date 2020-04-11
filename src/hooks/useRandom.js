46263c0d2476d37c09759d2c5a5b6badcbe97220
Janne Peippo <peippo@lumous.com>
2020-04-11 15:23:20 +0300

    Fix exhaustive deps warning

--- src/hooks/useRandom.js
+++ src/hooks/useRandom.js
@@ -7,13 +7,24 @@ const useRandom = (immediate = true) => {
 	const [application, setApplication] = useState(null);
 	const [error, setError] = useState(null);
 
-	const colors = [
-		{ back: "rgb(18, 74, 173)", text: "rgb(213, 160, 1)" },
-		{ back: "rgb(216, 216, 208)", text: "rgb(0, 0, 60)" },
-		{ back: "rgb(18, 20, 21)", text: "rgb(246, 159, 48)" },
-	];
+	// const fetchRandom = useCallback(() => {
+	// 	setApplication({
+	// 		reviewReasonCode: 2,
+	// 		reviewerComments: "Reviewer comment",
+	// 		customerMeaning: "Customer meaning",
+	// 		isApproved: false,
+	// 		plate: "TESTING$",
+	// 		color: { back: "rgb(18, 20, 21)", text: "rgb(246, 159, 48)" },
+	// 	});
+	// }, []);
 
 	const fetchRandom = useCallback(() => {
+		const colors = [
+			{ back: "rgb(18, 74, 173)", text: "rgb(213, 160, 1)" },
+			{ back: "rgb(216, 216, 208)", text: "rgb(0, 0, 60)" },
+			{ back: "rgb(18, 20, 21)", text: "rgb(246, 159, 48)" },
+		];
+
 		setIsFetching(true);
 		setApplication(null);
 		setError(null);
